5560453f95885ea0a7600ab77c30e448cc574823
xushengfeng <xushengfeng_zg@163.com>
2026-07-21 14:56:58 +0800

    主要 仅esm导出

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,8 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
-			name: "MydeUI",
-			formats: ["es", "cjs"],
-			fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
-		},
-		rollupOptions: {
-			external: ["dkh-ui"],
-			output: {
-				globals: {
-					"dkh-ui": "DkhUI",
-				},
-			},
+			formats: ["es"],
+			fileName: () => "index.js",
 		},
 	},
 });
